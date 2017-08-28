72964c179321cf44357d94501fdd8346f3f2a4eb
syafriyono <syafriyono7@gmail.com>
2017-08-28 13:45:24 +0700

    import for UpliftlabUniversity

---
+++ UpliftlabUniversity/DAL/SchoolContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+using UpliftlabUniversity.Models;
+
+namespace UpliftlabUniversity.DAL
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("Kuliah")
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Course> Course { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ UpliftlabUniversity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="UpliftlabUniversity.MvcApplication" Language="C#" %>
